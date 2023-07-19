4a5d2452ae94b18d542ae37d1261539c175859f6
Cheryl Lee <cheryl@open.gov.sg>
2023-07-19 12:49:13 +0800

    updated

--- frontend-easy/src/main.ts
+++ frontend-easy/src/main.ts
@@ -1,6 +1,7 @@
 // Import to run the side effect of setting base Urls before it is used elsewhere
 import './API';
 
+import Vue from 'vue';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
@@ -8,11 +9,10 @@ import { router } from './router';
 import App from './App.vue';
 import VueGtag from 'vue-gtag';
 
-Vue.use(VueGtag, {
-  config: { id: 'G-Q3G07BKKZ5' },
-});
-
 createApp(App)
   .use(router)
+  .use(VueGtag, {
+    config: { id: 'G-Q3G07BKKZ5' },
+  })
   .use(createPinia().use(piniaPluginPersistedstate))
   .mount('#app');
