3c3ef0ec43c2cf01e17499bad0c66879cd8dae2d
Cheryl Lee <cheryl@open.gov.sg>
2023-07-25 11:06:06 +0800

    feat: set up image API

--- backend/src/public/listings/listings.controller.ts
+++ backend/src/public/listings/listings.controller.ts
@@ -3,48 +3,86 @@ import {
   Controller,
   DefaultValuePipe,
   Get,
+  HttpStatus,
   Param,
+  ParseFilePipe,
+  ParseFilePipeBuilder,
   ParseIntPipe,
   Post,
   Query,
-} from '@nestjs/common'
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
 
 // import { CreateListingReq } from '~shared/types/api';
-import { ListingsService } from './listings.service'
+import { ListingsService } from "./listings.service";
+import { FileInterceptor } from "@nestjs/platform-express";
 
-@Controller('listing')
+@Controller("listing")
 export class ListingsController {
   constructor(private listingService: ListingsService) {}
 
-  @Get(':listingId')
-  async find(@Param('listingId', ParseIntPipe) listingId: number) {
-    const listing = await this.listingService.find(listingId)
+  @Get(":listingId")
+  async find(@Param("listingId", ParseIntPipe) listingId: number) {
+    const listing = await this.listingService.find(listingId);
     if (!listing) {
-      throw new Error('Listing not found')
+      throw new Error("Listing not found");
     }
 
-    return listing
+    return listing;
   }
 
   @Get()
   findAll(
-    @Query('categoryId', new DefaultValuePipe(0), ParseIntPipe)
-    categoryId?: number,
+    @Query("categoryId", new DefaultValuePipe(0), ParseIntPipe)
+    categoryId?: number
   ) {
-    return this.listingService.findAll(categoryId)
+    return this.listingService.findAll(categoryId);
   }
 
   // Can you help me enable global validation pipe so that it will actually validate the DTO, i cant figure out where is the validation pipe set
   // looks like logged validation pipe but no idea why it is not running validation and transformation
 
-  @Post('new/create')
+  @Post("new/create")
   // createListing(@Body() listing: CreateListingReq) {
   createListing(@Body() listing: any) {
     return this.listingService.createListing(
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      listing as unknown as CreateListingReq,
+      listing as unknown as CreateListingReq
+    );
+  }
+
+  @Post("file")
+  uploadFileAndPassValidation(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: "jpeg",
+        })
+        .addMaxSizeValidator({
+          maxSize: 1000,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        })
     )
+    file: Express.Multer.File
+  ) {
+    return {
+      file: file.buffer.toString(),
+    };
+  }
+}
+
+import { PipeTransform, Injectable, ArgumentMetadata } from "@nestjs/common";
+
+@Injectable()
+export class FileSizeValidationPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    // "value" is an object containing the file's attributes and metadata
+    const oneKb = 10000;
+    return value.size < oneKb;
   }
 }
