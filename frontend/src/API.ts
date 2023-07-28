e216a15d8c74fcae7bd862551efde66d5247d39c
Cheryl Lee <cheryl@open.gov.sg>
2023-07-28 22:15:51 +0800

    Fix Base API

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -1,14 +1,14 @@
-import { sf } from 'simpler-fetch';
+import { sf } from "simpler-fetch";
 
 /**
  * Export the list of identifiers for the different base Urls.
  */
 export const API = {
   /** Neutral Version, where this is the raw baseApiUrl without any postfix versioning */
-  vNeutral: 'vn',
+  vNeutral: "vn",
 
   /** Version 1 */
-  v1: 'v1',
+  v1: "v1",
 };
 
 /**
@@ -16,13 +16,13 @@ export const API = {
  * since most of the different base Urls will be for postfix versioning.
  */
 const baseApiUrl =
-  import.meta.env.MODE !== 'production'
-    ? 'http://localhost:3000'
+  import.meta.env.MODE !== "production"
+    ? "http://localhost:3000"
     : import.meta.env.VITE_API_URL;
 
 // Configure API library base Urls
 sf.addBase(API.vNeutral, baseApiUrl)
-  .addBase(API.v1, `api/v1`)
+  .addBase(API.v1, `${baseApiUrl}/api/v1`)
 
   // Defaults to v1
   .setDefault(API.v1);
